29ef02047285467e10a1051400730883cc2ea2c5
Ian Bruene <ianbruene@gmail.com>
2018-07-29 17:26:16 -0500

    Added tests for statestr.c.

--- tests/libntp/statestr.c
+++ tests/libntp/statestr.c
@@ -15,6 +15,31 @@ TEST_TEAR_DOWN(statestr) {}
 #include "ntp_control.h"
 
 
+// res_match_flags()
+TEST(statestr, ResMatchFlags) {
+	TEST_ASSERT_EQUAL_STRING("ntpport", res_match_flags(RESM_NTPONLY));
+}
+
+// res_access_flags()
+TEST(statestr, ResAccessFlags) {
+	TEST_ASSERT_EQUAL_STRING("notrust", res_access_flags(RES_DONTTRUST));
+}
+
+// k_st_flags()
+TEST(statestr, KSTFlags) {
+	TEST_ASSERT_EQUAL_STRING("pll", k_st_flags(STA_PLL));
+}
+
+// statustoa
+TEST(statestr, StatusToA) {
+	TEST_ASSERT_EQUAL_STRING("leap_add_sec, sync_22, 7 events, no_sys_peer",
+							 statustoa(TYPE_SYS, 0x12345678));
+	TEST_ASSERT_EQUAL_STRING("authenb, reach, sel_sys.peer, 7 events, access_denied",
+							 statustoa(TYPE_PEER, 0x12345678));
+	TEST_ASSERT_EQUAL_STRING("7 events, clk_8",
+							 statustoa(TYPE_CLOCK, 0x12345678));
+}
+
 // eventstr()
 TEST(statestr, PeerRestart) {
 	TEST_ASSERT_EQUAL_STRING("restart", eventstr(PEVNT_RESTART));
@@ -34,6 +59,10 @@ TEST(statestr, ClockCodeUnknown) {
 }
 
 TEST_GROUP_RUNNER(statestr) {
+	RUN_TEST_CASE(statestr, ResMatchFlags);
+	RUN_TEST_CASE(statestr, ResAccessFlags);
+	RUN_TEST_CASE(statestr, KSTFlags);
+	RUN_TEST_CASE(statestr, StatusToA);
 	RUN_TEST_CASE(statestr, PeerRestart);
 	RUN_TEST_CASE(statestr, SysUnspecified);
 	RUN_TEST_CASE(statestr, ClockCodeExists);
